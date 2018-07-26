c699a4c4534e32f0794097f1230efd1b34930dfb
James Kamana <jaykamana@gmail.com>
2018-07-27 00:46:16 +0300

    Add Auth middleaware to routes

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,32 @@
 |
  */
 
+
+Auth::routes();
+
+
 Route::get('/', function () {
   return view('welcome');
 });
 
-Auth::routes();
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
 
-Route::get('/home', 'HomeController@index');
+  Route::get('/home', [
+    'uses' => 'HomeController@index',
+    'as' => 'home'
+  ]);
 
-Route::get('/post/create', [
-  'uses' => 'PostsController@create',
-  'as' => 'post.create'
-]);
+  Route::get('/post/create', [
+    'uses' => 'PostsController@create',
+    'as' => 'post.create'
+  ]);
 
-Route::post('/post/store', [
-  'uses' => 'PostsController@store',
-  'as' => 'post.store'
-]);
+  Route::post('/post/store', [
+    'uses' => 'PostsController@store',
+    'as' => 'post.store'
+  ]);
+
+});
+
+
 
