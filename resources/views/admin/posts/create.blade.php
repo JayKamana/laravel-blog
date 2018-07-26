9d140ef194d2c7e0cb1283fcddafb3182da59308
James Kamana <jaykamana@gmail.com>
2018-07-26 04:45:13 +0300

    Initial commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+  <h2 class="text-center">
+    Create new Post
+  </h2>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+  
+    </div>
+    <div class="panel-body">
+      <form action="/post/store" method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input id="title" type="text" name="title" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="featured">Featured</label>
+          <input id="featured" type="file" name="featured" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="content">Content</label>
+          <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+          <div class="text-center">
+            <button class="btn btn-success" type="submit">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+@stop
