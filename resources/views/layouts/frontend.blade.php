f1b7f06d1174c9f7594e8b2ea62137086e9f5460
James Kamana <jaykamana@gmail.com>
2018-10-14 13:36:03 +0200

    Add share buttons using AddThis

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -155,8 +155,8 @@
 <script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
 <script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
 
-
 <!-- ...end JS Script -->
 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5bc25a3baa3ec68a"></script>
 </body>
 </html>
